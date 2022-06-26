3511d31e233b5cc60de90472d737385a2c0a085c
ronnie <ironniehofmann@gmail.com>
2022-06-26 17:16:32 +0200

    all inclusive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Meal from './Meal'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        <h1 className="font-bold text-4xl lg:text-6xl mt-10">
+          Random meal generator
+        </h1>
+        <div className="underline mx-auto mt-4 mb-5"></div>
+        <Meal />
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
